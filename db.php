8bc3fe029da2aa10916abac03324fbe6c2e3b722
ranjeetpatil6760 <30472237+ranjeetpatil6760@users.noreply.github.com>
2018-11-30 16:16:59 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,10 @@
-
 <?php
 
 $con=mysqli_connect("localhost","root","","salavi");
+mysqli_query($con,"SET character_set_results=utf8");
+mb_language('uni');
+mb_internal_encoding('UTF-8');
+
 if (mysqli_connect_errno())
 {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
