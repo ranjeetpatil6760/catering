40079105dcbe13c95fed700c1e86fbb0024c10e6
sajida2406 <42533500+sajida2406@users.noreply.github.com>
2018-12-26 12:50:39 +0530

    Add files via upload

---
+++ cat_dues.php
@@ -0,0 +1,331 @@
+<?php
+include "db.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<!-- start: Meta -->
+	<meta charset="utf-8">
+	<title>Customer Dues</title>
+	<meta name="description" content="Bootstrap Metro Dashboard">
+	<meta name="author" content="Dennis Ji">
+	<meta name="keyword" content="Metro, Metro UI, Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+	<!-- end: Meta -->
+	
+	<!-- start: Mobile Specific -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- end: Mobile Specific -->
+	
+	<!-- start: CSS -->
+	<link id="bootstrap-style" href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link id="base-style" href="css/style.css" rel="stylesheet">
+	<link id="base-style-responsive" href="css/style-responsive.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&subset=latin,cyrillic-ext,latin-ext' rel='stylesheet' type='text/css'>
+	<!-- end: CSS -->
+	
+
+	<!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<link id="ie-style" href="css/ie.css" rel="stylesheet">
+	<![endif]-->
+	
+	<!--[if IE 9]>
+		<link id="ie9style" href="css/ie9.css" rel="stylesheet">
+	<![endif]-->
+		
+	<!-- start: Favicon -->
+	<link rel="shortcut icon" href="img/favicon.ico">
+	<!-- end: Favicon -->
+	
+		
+		
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>	
+</head>
+
+<body>
+		<!-- start: Header -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<div>
+          <?php include 'header.php';?>
+	
+	</div>
+	<!-- start: Header -->
+	
+		<div class="container-fluid-full">
+		<div class="row-fluid">
+				
+			<!-- start: Main Menu -->
+			<?php include 'sidebar.html';?>
+			<!-- end: Main Menu -->
+			
+			<noscript>
+				<div class="alert alert-block span10">
+					<h4 class="alert-heading">Warning!</h4>
+					<p>You need to have <a href="http://en.wikipedia.org/wiki/JavaScript" target="_blank">JavaScript</a> enabled to use this site.</p>
+				</div>
+			</noscript>
+			
+			<!-- start: Content -->
+			<div id="content" class="span10">
+			
+			
+			<ul class="breadcrumb">
+				<li>
+					<i class="icon-home"></i>
+					<a href="index.php">Home</a>
+					<i class="icon-angle-right"></i> 
+				</li>
+				<li>
+					<i class="icon-edit"></i>
+					<a href="#">Customer Dues</a>
+				</li>
+			</ul>
+			
+	<!-- 		<div class="row-fluid sortable">
+				<div class="box span12">
+					<div class="box-header" data-original-title>
+						<h2><i class="halflings-icon white edit"></i><span class="break"></span>Form Elements</h2>
+						<div class="box-icon">
+							<a href="#" class="btn-setting"><i class="halflings-icon white wrench"></i></a>
+							<a href="#" class="btn-minimize"><i class="halflings-icon white chevron-up"></i></a>
+							<a href="#" class="btn-close"><i class="halflings-icon white remove"></i></a>
+						</div>
+					</div>
+					<div class="box-content">
+						<form class="form-horizontal">
+						  <fieldset>
+							<div class="control-group">
+							  <label class="control-label" for="typeahead">Auto complete </label>
+							  <div class="controls">
+								<input type="text" class="span6 typeahead" id="typeahead"  data-provide="typeahead" data-items="4" data-source='["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]'>
+								<p class="help-block">Start typing to activate auto complete!</p>
+							  </div>
+							</div>
+							<div class="control-group">
+							  <label class="control-label" for="date01">Date input</label>
+							  <div class="controls">
+								<input type="text" class="input-xlarge datepicker" id="date01" value="02/16/12">
+							  </div>
+							</div>
+
+							<div class="control-group">
+							  <label class="control-label" for="fileInput">File input</label>
+							  <div class="controls">
+								<input class="input-file uniform_on" id="fileInput" type="file">
+							  </div>
+							</div>          
+							<div class="control-group hidden-phone">
+							  <label class="control-label" for="textarea2">Textarea WYSIWYG</label>
+							  <div class="controls">
+								<textarea class="cleditor" id="textarea2" rows="3"></textarea>
+							  </div>
+							</div>
+							<div class="form-actions">
+							  <button type="submit" class="btn btn-primary">Save changes</button>
+							  <button type="reset" class="btn">Cancel</button>
+							</div>
+						  </fieldset>
+						</form>   
+
+
+
+
+
+					</div>
+				</div>-->
+
+				<?php
+				$ids=$_GET['id'];
+
+$result = mysqli_query($con,"SELECT * FROM cat_bill where c_id='$ids'");
+ while($row = mysqli_fetch_array($result))
+{
+				?>
+
+			<div class="row-fluid sortable">
+				<div class="box span12">
+					<div class="box-header" data-original-title>
+						<h2><i class="halflings-icon white edit"></i><span class="break"></span>Customer Dues</h2>
+						<div class="box-icon">
+							
+							<a href="#" class="btn-minimize"><i class="halflings-icon white chevron-up"></i></a>
+							<a href="#" class="btn-close"><i class="halflings-icon white remove"></i></a>
+						</div>
+					</div>
+					<div class="box-content">
+						<form class="form-horizontal" action="" method="POST">
+							<fieldset>
+								<br>
+								 <div class="control-group">
+								<label class="control-label" for="focusedInput">Pending Amount:</label>
+								<div class="controls">
+								  <input class="input-xlarge focused" id="pending" type="text" placeholder="Name" readonly="true" name="Pending" value="<?php echo $row['dues']; }?>">
+								</div><br>
+								   <div class="control-group">
+								<label class="control-label" for="focusedInput">Paid:</label>
+								<div class="controls">
+								<input class="input-xlarge focused" id="paid" type="text" placeholder="Rs." name="paid">
+							</div><br>
+								<div class="control-group">
+								<label class="control-label" for="focusedInput">Remaining Dues:</label>
+								<div class="controls">
+								<input class="input-xlarge focused" id="new_dues" type="text" placeholder="Rs." name="new_dues">
+								</div>
+
+								<br>
+					
+
+								<div class="control-group">
+								<label class="control-label" for="selectError">Payment Status:</label>
+								<div class="controls">
+									<div id="itemRows">
+
+								  <select id="selectError" data-rel="chosen" size="4" name="p_status">
+								  	<option>Full Payment</option>
+								  	<option>Dues</option>
+							  </div>
+						</select>
+					</div>
+				</div>
+			</div>
+
+			
+							  <div class="form-actions" >
+								<button type="submit" class="btn btn-primary" name="save">Save </button>
+								<button class="btn">Cancel</button>
+							  </div>
+							</fieldset>
+						  </form>
+						  <script type="text/javascript">
+						  $(document).ready(function(){
+
+    $('#paid').change(function(){
+    	var t=$('#pending').val();
+    	var a=$('#paid').val();
+    	var e=t-a;
+    	if()
+        $('#new_dues').val(e);
+    });
+});
+						  </script>
+
+						  <?php
+
+
+if(isset($_POST['save']))
+{
+
+$n_d=$_POST['new_dues'];
+$p_s=$_POST['p_status'];
+
+//inserting in hotel_customer_info
+$qry="Update cat_bill set dues='$n_d',pay_status='$p_s' where c_id='$ids'";
+if(mysqli_query($con,$qry))
+{
+	echo "<script> alert('Updates successfully')";
+	echo " </script>";
+}
+else
+{
+	echo "<script> alert('Failed to update.Try Again!')";
+	echo " </script>";
+}
+}
+?>
+					
+					</div>
+		
+				</div><!--/span-->
+			
+			</div><!--/row-->
+			
+			
+
+	</div><!--/.fluid-container-->
+	
+			<!-- end: Content -->
+		</div><!--/#content.span10-->
+		</div><!--/fluid-row-->
+		
+	<div class="modal hide fade" id="myModal">
+		<div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal">×</button>
+			<h3>Settings</h3>
+		</div>
+		<div class="modal-body">
+			<p>Here settings can be configured...</p>
+		</div>
+		<div class="modal-footer">
+			<a href="#" class="btn" data-dismiss="modal">Close</a>
+			<a href="#" class="btn btn-primary" name="save">Save changes</a>
+		</div>
+	</div>
+	
+	<div class="clearfix"></div>
+	
+	 <?php include 'footer.html';?>
+	
+	<!-- start: JavaScript-->
+
+		<script src="js/jquery-1.9.1.min.js"></script>
+	<script src="js/jquery-migrate-1.0.0.min.js"></script>
+	
+		<script src="js/jquery-ui-1.10.0.custom.min.js"></script>
+	
+		<script src="js/jquery.ui.touch-punch.js"></script>
+	
+		<script src="js/modernizr.js"></script>
+	
+		<script src="js/bootstrap.min.js"></script>
+	
+		<script src="js/jquery.cookie.js"></script>
+	
+		<script src='js/fullcalendar.min.js'></script>
+	
+		<script src='js/jquery.dataTables.min.js'></script>
+
+		<script src="js/excanvas.js"></script>
+	<script src="js/jquery.flot.js"></script>
+	<script src="js/jquery.flot.pie.js"></script>
+	<script src="js/jquery.flot.stack.js"></script>
+	<script src="js/jquery.flot.resize.min.js"></script>
+	
+		<script src="js/jquery.chosen.min.js"></script>
+	
+		<script src="js/jquery.uniform.min.js"></script>
+		
+		<script src="js/jquery.cleditor.min.js"></script>
+	
+		<script src="js/jquery.noty.js"></script>
+	
+		<script src="js/jquery.elfinder.min.js"></script>
+	
+		<script src="js/jquery.raty.min.js"></script>
+	
+		<script src="js/jquery.iphone.toggle.js"></script>
+	
+		<script src="js/jquery.uploadify-3.1.min.js"></script>
+	
+		<script src="js/jquery.gritter.min.js"></script>
+	
+		<script src="js/jquery.imagesloaded.js"></script>
+	
+		<script src="js/jquery.masonry.min.js"></script>
+	
+		<script src="js/jquery.knob.modified.js"></script>
+	
+		<script src="js/jquery.sparkline.min.js"></script>
+	
+		<script src="js/counter.js"></script>
+	
+		<script src="js/retina.js"></script>
+
+		<script src="js/custom.js"></script>
+	<!-- end: JavaScript-->
+	
+</body>
+</html>
