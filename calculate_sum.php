cc32e5e0c75c642664abc04580910941bce34934
sajida2406 <42533500+sajida2406@users.noreply.github.com>
2018-12-26 13:03:26 +0530

    Add files via upload

---
+++ calculate_sum.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php	include "db.php";?>
+	<!-- start: Meta -->
+	<meta charset="utf-8">
+	<title>Hotel Inventory</title>
+	<meta name="description" content="Bootstrap Metro Dashboard">
+	<meta name="author" content="Dennis Ji">
+	<meta name="keyword" content="Metro, Metro UI, Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<!-- end: Meta -->
+	
+	<!-- start: Mobile Specific -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- end: Mobile Specific -->
+	
+	<!-- start: CSS -->
+	<link id="bootstrap-style" href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link id="base-style" href="css/style.css" rel="stylesheet">
+	<link id="base-style-responsive" href="css/style-responsive.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&subset=latin,cyrillic-ext,latin-ext' rel='stylesheet' type='text/css'>
+	<!-- end: CSS -->
+	
+<style type="text/css">
+    form{
+        margin: 20px 0;
+    }
+    form input, button{
+        padding: 5px;
+    }
+    table{
+        width: 100%;
+        margin-bottom: 20px;
+		border-collapse: collapse;
+    }
+    table, th, td{
+        border: 1px solid #cdcdcd;
+    }
+    table th, table td{
+        padding: 10px;
+        text-align: left;
+    }
+</style>
+	<!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<link id="ie-style" href="css/ie.css" rel="stylesheet">
+	<![endif]-->
+	
+	<!--[if IE 9]>
+		<link id="ie9style" href="css/ie9.css" rel="stylesheet">
+	<![endif]-->
+		
+	<!-- start: Favicon -->
+	<link rel="shortcut icon" href="img/favicon.ico">
+	<!-- end: Favicon -->
+	
+	<script src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript">
+
+    $(document).ready(function(){
+        $(".add-row").click(function(){
+        	var item = $("#item").val();
+             
+            var quantity = $("#quantity").val();
+            var unit = $("#unit").val();
+            var price = $("#price").val();
+            var dues = $("#dues").val();
+           
+            var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + item + "</td><td>"+ quantity + "</td><td>" + unit + "</td><td>" + price + "</td><td>" + dues + "</td></tr>";
+            $("table tbody").append(markup);
+            
+        
+        });
+        
+
+        function calculateSum() {
+    var sum = 0;
+    //iterate through each textboxes and add the values
+    $(".txt").each(function () {
+
+        //add only if the value is number
+        if (!isNaN(this.value) && this.value.length != 0) {
+            sum += parseFloat(this.value);
+        }
+
+    });
+    //.toFixed() method will roundoff the final sum to 2 decimal places
+    $("#sum").html(sum.toFixed(2));
+}
+
+$(".addRow").on("click", addRow);
+
+$("table tbody").on("keyup", ".txt", function () {
+    calculateSum();
+});
+
+
+        // Find and remove selected table rows
+        $(".delete-row").click(function(){
+            $("table tbody").find('input[name="record"]').each(function(){
+            	if($(this).is(":checked")){
+                    $(this).parents("tr").remove();
+                }
+            });
+        });
+    });    
+</script>
+		
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>	
+</head>
+
+<body>
+
+
+		<!-- start: Header -->
+	<div>
+          <?php include 'header.php';?>
+	
+	</div>
+	<!-- start: Header -->
+	
+		<div class="container-fluid-full">
+		<div class="row-fluid">
+				
+			<!-- start: Main Menu -->
+			<?php include 'sidebar.html';?>
+			<!-- end: Main Menu -->
+			
+			<noscript>
+				<div class="alert alert-block span10">
+					<h4 class="alert-heading">Warning!</h4>
+					<p>You need to have <a href="http://en.wikipedia.org/wiki/JavaScript" target="_blank">JavaScript</a> enabled to use this site.</p>
+				</div>
+			</noscript>
+			
+			<!-- start: Content -->
+			<div id="content" class="span10">
+			
+			
+			<ul class="breadcrumb">
+				<li>
+					<i class="icon-home"></i>
+					<a href="index.html">Home</a>
+					<i class="icon-angle-right"></i> 
+				</li>
+				<li>
+					<i class="icon-edit"></i>
+					<a href="#">Add Items</a>
+				</li>
+			</ul>
+
+			
+	
+               
+			<div class="row-fluid sortable">
+				<div class="box span12">
+					<div class="box-header" data-original-title>
+						<h2><i class="halflings-icon white edit"></i><span class="break"></span>Add item to list</h2>
+						<div class="box-icon">
+							
+							<a href="#" class="btn-minimize"><i class="halflings-icon white chevron-up"></i></a>
+							<a href="#" class="btn-close"><i class="halflings-icon white remove"></i></a>
+						</div>
+					</div>
+					   <form action="" method="post">
+<fieldset>
+		
+					<div class="box-content">
+    	<input type="date" name="date"><br><br>
+    	<select id="item" data-rel="chosen" size="1" name="i_name">
+<?php
+
+include "db.php";
+								  $q1= "SELECT * FROM item_list";
+$result = mysqli_query($con,$q1) or die (mysql_error());
+while($row=mysqli_fetch_array($result,MYSQLI_ASSOC))
+{
+	?>
+	<option><?php echo $row['items']?></option>
+<?php
+}
+?>
+
+
+								  </select>
+        <input type="text" id="quantity" placeholder="Quantity">
+        <select id="unit" data-rel="chosen" name="Unit">
+	<option>kg</option>
+	<option>gm</option>
+	<option>mg</option>
+	<option>ltr</option>
+	<option>nag</option>
+	<option>dozen</option>
+</select>
+        <input type="text" id="price" placeholder="Price" class="txt">
+        <input id="dues" type="text" placeholder="Dues" class="txt"  >
+    	<input type="button" class="add-row btn btn-primary" value="Add Row">
+    </form>
+    <table>
+        <thead>
+            <tr>
+                <th>Select</th>
+                <th>Item</th>
+                <th>Quantity</th>
+                <th>Unit</th>
+                <th>Price</th>
+                <th>Dues</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+               
+            </tr>
+        </tbody>
+    </table>
+    <button type="button" class="delete-row btn btn-primary">Delete Row</button><br><br>
+                 	         
+				 <input type="text" name="Subtotal" id="sum" >	 
+				 <input type="text" name="total">				 
+					
+		<div class="form-actions" >
+		
+			<input type="submit" name="but" value="Submit"/>
+								
+								<button class="btn" name="save">Cancel</button>
+									<!--/span--></fieldset>
+			</form>
+									<?php
+									include "db.php";
+
+									if(isset($_POST['but']))
+									{
+
+									$date= $_POST['date'];
+									$Subtotal= $_POST['Subtotal'];
+									$tt = $_POST['total'];
+									
+$qry="INSERT INTO stock_hotel_details (date, subtotal, total_dues) VALUES ('$date','$Subtotal', '$tt')";
+if(mysqli_query($con,$qry) > 0 ){
+	echo "success";
+}
+	else{ 
+	echo "failed";
+}
+
+}
+
+									?>
+									</form>
+
+
+		</div>
+							  </div>
+						
+
+			
+		
+			
+</div>
+
+	<!--/.fluid-container-->
+	
+			</div><!-- end: Content -->
+		</div><!--/#content.span10-->
+		</div><!--/fluid-row-->
+		
+	
+	<div class="clearfix"></div>
+	<div>
+	 <?php include 'footer.html'; ?>
+	</div>
+	<!-- start: JavaScript-->
+
+	</div>	<script src="js/jquery-1.9.1.min.js"></script>
+	<script src="js/jquery-migrate-1.0.0.min.js"></script>
+	
+		<script src="js/jquery-ui-1.10.0.custom.min.js"></script>
+	
+		<script src="js/jquery.ui.touch-punch.js"></script>
+	
+		<script src="js/modernizr.js"></script>
+	
+		<script src="js/bootstrap.min.js"></script>
+	
+		<script src="js/jquery.cookie.js"></script>
+	
+		<script src='js/fullcalendar.min.js'></script>
+	
+		<script src='js/jquery.dataTables.min.js'></script>
+
+		<script src="js/excanvas.js"></script>
+	<script src="js/jquery.flot.js"></script>
+	<script src="js/jquery.flot.pie.js"></script>
+	<script src="js/jquery.flot.stack.js"></script>
+	<script src="js/jquery.flot.resize.min.js"></script>
+	
+		<script src="js/jquery.chosen.min.js"></script>
+	
+		<script src="js/jquery.uniform.min.js"></script>
+		
+		<script src="js/jquery.cleditor.min.js"></script>
+	
+		<script src="js/jquery.noty.js"></script>
+	
+		<script src="js/jquery.elfinder.min.js"></script>
+	
+		<script src="js/jquery.raty.min.js"></script>
+	
+		<script src="js/jquery.iphone.toggle.js"></script>
+	
+		<script src="js/jquery.uploadify-3.1.min.js"></script>
+	
+		<script src="js/jquery.gritter.min.js"></script>
+	
+		<script src="js/jquery.imagesloaded.js"></script>
+	
+		<script src="js/jquery.masonry.min.js"></script>
+	
+		<script src="js/jquery.knob.modified.js"></script>
+	
+		<script src="js/jquery.sparkline.min.js"></script>
+	
+		<script src="js/counter.js"></script>
+	
+		<script src="js/retina.js"></script>
+
+		<script src="js/custom.js"></script>
+	<!-- end: JavaScript-->
+	
+</body>
+</html>
