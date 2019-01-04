52d6697565edb97e773a2e3a7760e77d111ad4a7
sajida2406 <42533500+sajida2406@users.noreply.github.com>
2019-01-04 16:03:14 +0530

    Update w_new.php

--- w_new.php
+++ w_new.php
@@ -1,30 +1,34 @@
+
+
+<script src="js/jquery-1.9.1.min.js"></script>
+<script src='js/fullcalendar.min.js'></script>
 <?php include 'header.php';?>
 <?php include "db.php";?>
 <script src="js/jquery-1.9.1.min.js"></script>
 <script src='js/fullcalendar.min.js'></script>
-   
+   <form action="" method="post">
       <main class="main-content bgc-grey-100">
         <div id="mainContent">
           <div class="container-fluid">
-          	<h4 class="c-grey-900 mT-10 mB-30">Add Workers to Events </h4>
+            <h4 class="c-grey-900 mT-10 mB-30">Add Workers to Events </h4>
 
-          	 <div class="row">
-          	 	<div class="col-md-4">
-      								<label class="fw-500">Date</label>
-      								<div class=" form-group col-md-8">
-      									<div class="input-group">
-      										<div class="input-group-addon bgc-white bd bdwR-0"><i class="ti-calendar"></i></div>
-      										<?PHP
-											$date=date("Y-m-d");
+             <div class="row">
+              <div class="col-md-4">
+                      <label class="fw-500">Date</label>
+                      <div class=" form-group col-md-8">
+                        <div class="input-group">
+                          <div class="input-group-addon bgc-white bd bdwR-0"><i class="ti-calendar"></i></div>
+                          <?PHP
+                      $date=date("Y-m-d");
 
-											echo"<input type='date' name='date' id='datepicker' value='$date' required >";?>
-      									</div>
-      								</div>
-      							</div>
+                      echo"<input type='date' name='date' id='datepicker' value='$date' required >";?>
+                        </div>
+                      </div>
+                    </div>
 
-      						</div>
+                  </div>
 
-<form>
+
 <div class="form-row">
 
 <?php
@@ -57,7 +61,7 @@ $result1 = mysqli_query($con,"SELECT * FROM event_info where c_id='$ids'");
 
 <div class="control-group">
  
-	<div id="itemRows">
+  <div id="itemRows">
 <div class="form-row">
 
 <div class="form-group col-md-2"> 
@@ -66,7 +70,7 @@ $result1 = mysqli_query($con,"SELECT * FROM event_info where c_id='$ids'");
 
 <select id="selectError" class="form-control" data-rel="chosen" name="w_name">
 
-	
+  
 
 
                 <?php
@@ -76,8 +80,8 @@ $q1= "SELECT * FROM h_worker_info";
 $result = mysqli_query($con,$q1);
 while($row=mysqli_fetch_array($result,MYSQLI_ASSOC))
 {
-	?>
-	<option><?php echo $row['w_name']?></option>
+  ?>
+  <option><?php echo $row['w_name']?></option>
 <?php
 }
 ?>
@@ -89,17 +93,17 @@ while($row=mysqli_fetch_array($result,MYSQLI_ASSOC))
 
 
 <div class="form-group col-md-2">
- 	<label class="fw-500">No. of Workers</label>
+  <label class="fw-500">No. of Workers</label>
 
 <input type="text" class="form-control" name="no_w" size="4" />
-</div>	
+</div>  
 
 
 <div class="form-group col-md-2">
- 	<label class="fw-500">Male</label>
+  <label class="fw-500">Male</label>
 
 <input type="text" class="form-control" name="male" />
-</div>	
+</div>  
 
 <div class="form-group col-md-2">
   <label class="fw-500">Female</label>
@@ -121,14 +125,9 @@ while($row=mysqli_fetch_array($result,MYSQLI_ASSOC))
 
 
 <div class="form-group col-md-2">
-	<label class="fw-500">Add Workers</label>
+  <label class="fw-500">Add Workers</label>
 <input onclick="addRow(this.form);" class="btn btn-primary form-control" type="button" value="Add Workers" />
-</div>	
 
-
-
-<script src="js/jquery-1.9.1.min.js"></script>
-<script src='js/fullcalendar.min.js'></script>
 <script type="text/javascript">
 var rowNum = 0;
 var sum=0;
@@ -158,20 +157,20 @@ function removeRow(rnum) {
 jQuery('#rowNum'+rnum).remove();
 }
 
-$(document).ready(function(){
-    $('#dcnt').change(function(){
-    	var s=$('#stotal').val();
-    	var d=$('#dcnt').val();
-    	var a=s-((s*d)/100);
-        $('#total').val(a);
-    });
-});
+//$(document).ready(function(){
+  //  $('#dcnt').change(function(){
+    //  var s=$('#stotal').val();
+      //var d=$('#dcnt').val();
+      //var a=s-((s*d)/100);
+        //$('#total').val(a);
+   // });
+//});
 
 </script>
-<?php 
-   $phpVar =  $_COOKIE['flag'];
-   
-   ?>
+</div>  
+
+
+
 </div>
 </div>
 </div>
@@ -196,19 +195,10 @@ $(document).ready(function(){
                 
 <div class="form-row">
 <div class="form-group col-md-6">
-	<button type="submit" class="btn cur-p btn-success" name="save">Save changes</button> |<button class="btn cur-p btn-danger">Cancel</button>
-</div>	
+  <button type="submit" class="btn cur-p btn-success" name="save">Save changes</button> |<button class="btn cur-p btn-danger">Cancel</button>
+</div>  
          
-           
-          
-
-</div>
-</div>
-</form>
-</div>
-</div>
-</main>
-             <?php
+ <?php
 include "db.php";
 //$id1 = mysql_query("SELECT MAX(h_id) FROM hotel_customer_info");
 //$id = "SELECT * FROM hotel_customer_info WHERE h_id = $id1 ";
@@ -242,10 +232,20 @@ foreach($_POST['wname'] as $cnt => $a) {
 $sql = "INSERT INTO event_worker (w_name, c_id, wqty,male,female,w_dues,w_amount) VALUES ('$a', '$ids', '".$_POST['no_w'][$cnt]."', '".$_POST['male'][$cnt]."', '".$_POST['female'][$cnt]."','".$_POST['dues'][$cnt]."','".$_POST['amt'][$cnt]."')";
 mysqli_query($con,$sql);
 }
-unset($_COOKIE['flag']);
-$phpVar=0;
+
+
 }
 ?>
+           
+          
+
+</div>
+</div>
+
+</div>
+</div>
+</main>
+            
 <!-- <?php
 
 
@@ -278,5 +278,5 @@ $phpVar=0;
 ?> -->
 
 
-
+</form>
 <?php include 'footer.php';?>
